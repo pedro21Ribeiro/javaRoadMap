6f3111158167665b2d6edbfbfcf6fda764c2cf31
Pedro Silva Ribeiro <pedr21ribeiro7@gmail.com>
2024-08-03 09:02:12 -0300

    Updated ExceptionHandling
    Colocado exception hendling para caso de um numero sem integer

--- excepitonHandling/Main.java
+++ excepitonHandling/Main.java
@@ -3,21 +3,43 @@ import java.util.Scanner;
 import net.pedro.Age;
 import net.pedro.exception.BurroException;
 
+/* 
+ * Exit 0 - Sem problemas
+ * Exit 1 - Problema de Conversão
+ * Exit 2 - Problema de Burrice
+ * Exit 3 - Erro desconhecido
+*/
+
+
 public class Main {
 
     public static void main(String[] args){
         Scanner myScanner = new Scanner(System.in);    
+        int idade = 0;
 
         System.out.print("Digite uma idade a ser validada: ");
-        int idade = Integer.parseInt(myScanner.nextLine());
+        try{
+            idade = Integer.parseInt(myScanner.nextLine());
+        }catch(NumberFormatException e){
+            System.out.println("O input digitado não é um inteiro");
+            System.exit(1);
+        }catch(Exception e){
+            System.out.println("Ocorreu um erro");
+            System.out.println(e);
+            System.exit(3);
+        }
 
         try{
             Age.validateAge(idade);
-            System.out.println("Programa rodou sem erros");
         }catch(BurroException e){
-            System.out.println("Ocorreu um erro de burrice no programa: " + e.getMessage());
+            System.out.println("Ocorreu um erro de burrice durante a execução do programa: " + e.getMessage());
+            System.exit(2);
         }finally{
             myScanner.close();
         }
+
+        
+        System.out.println("Programa rodou sem erros");
+        System.exit(0);
     }
 }
