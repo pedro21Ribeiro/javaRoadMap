84bcab0fdf5cc8b52ff1594dcfcd777369c906f7
Pedro Silva Ribeiro <pedr21ribeiro7@gmail.com>
2024-08-03 08:49:59 -0300

    Adicionada error handling no programa Usando try, catch & finally

--- excepitonHandling/Main.java
+++ excepitonHandling/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 import net.pedro.Age;
+import net.pedro.exception.BurroException;
 
 public class Main {
 
@@ -9,9 +10,14 @@ public class Main {
 
         System.out.print("Digite uma idade a ser validada: ");
         int idade = Integer.parseInt(myScanner.nextLine());
-        Age.validateAge(idade);
-        System.out.println("Programa rodou sem erros");
 
-        myScanner.close();
+        try{
+            Age.validateAge(idade);
+            System.out.println("Programa rodou sem erros");
+        }catch(BurroException e){
+            System.out.println("Ocorreu um erro de burrice no programa: " + e.getMessage());
+        }finally{
+            myScanner.close();
+        }
     }
 }
