43d18b0c67651d4b86dc45581ce269331ebc9528
Pedro Silva Ribeiro <pedr21ribeiro7@gmail.com>
2024-08-03 10:33:01 -0300

    Adicionado o hashMap ao Main.java

--- dataStruct/Main.java
+++ dataStruct/Main.java
@@ -1,13 +1,14 @@
 package dataStruct;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import dataStruct.Objetos.Pessoa;
 
 public class Main {
 
     public static void main(String[] args) {
-        arrayList(); 
+        hashMap();
     }
     
     private static void arrayList(){
@@ -61,6 +62,51 @@ public class Main {
         System.out.println("------ Pós deleção ---------");
         pessoas.forEach(i -> {
             System.out.println("Nome : " + i.getNome() + " Profissão: "+ i.getProfissao() + " Salario: " + i.getSalario());
+        });
+    }
+
+    private static void hashMap(){
+       HashMap<String, Pessoa> pessoas = new HashMap<String, Pessoa>(); 
+       HashMap<String, Pessoa> pessoas2 = new HashMap<String, Pessoa>(); 
+
+        //Adicionando
+        pessoas.put("Raimundo",new Pessoa("Raimundo", 15));
+        pessoas.put("Abner",new Pessoa("Abner", 17));
+        pessoas.put("Pedro",new Pessoa("Pedro", 20));
+        pessoas.put("João",new Pessoa("João", 16));
+
+        //Adicionando a pessoas2
+        pessoas2.put("Mariane",new Pessoa("Mariane", 20));
+        pessoas2.put("Adriana",new Pessoa("Adriana", 50));
+        pessoas2.put("Marcos",new Pessoa("Marcos", 46));
+        pessoas2.put("Abner",new Pessoa("Abner", 17));
+
+        //Pegando Usando chaves
+        System.out.println(pessoas.get("Raimundo").getNome());;
+        System.out.println(pessoas2.get("Marcos").getNome());;
+
+        System.out.println("----------- Chaves de Pessoas ----------");
+        //Pegando as todas as chave
+        for(String i : pessoas.keySet()){
+            System.out.println(i);
+        }
+
+        System.out.println("----------- Chaves de Pessoas2 ----------");
+        //Pegando as todas as chave
+        for(String i : pessoas2.keySet()){
+            System.out.println(i);
+        }
+
+        //deletando um de cada
+        pessoas.remove("Raimundo");
+        pessoas2.remove("Marcos");
+
+        //Unindo os dois sets em um só, experado que o abner que tem chave repetida se una ao outro abner
+        HashMap<String, Pessoa> unido = new HashMap<String, Pessoa>(pessoas);
+        unido.putAll(pessoas2);
+
+        unido.forEach( (chave, pessoa) -> {
+            System.out.println("Chave : " + chave + " | Objeto :" + pessoa);
         });
     }
 }
